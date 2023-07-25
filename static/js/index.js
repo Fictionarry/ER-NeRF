a525d521820fab1dff22ed10d635fbd80d9f3b5e
unknown <571680391@qq.com>
2023-07-25 16:16:47 +0800

    update js

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,23 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+// var INTERP_BASE = "./static/interpolation/stacked";
+// var NUM_INTERP_FRAMES = 240;
 
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
+// var interp_images = [];
+// function preloadInterpolationImages() {
+//   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
+//     var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+//     interp_images[i] = new Image();
+//     interp_images[i].src = path;
+//   }
+// }
 
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
+// function setInterpolationImage(i) {
+//   var image = interp_images[i];
+//   image.ondragstart = function() { return false; };
+//   image.oncontextmenu = function() { return false; };
+//   $('#interpolation-image-wrapper').empty().append(image);
+// }
 
 
 $(document).ready(function() {
