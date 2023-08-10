217c61a7e36e48d21fcee2d6c196dd800f1f39e5
user <571680391@qq.com>
2023-08-10 15:21:33 +0800

    update script

--- scripts/train_obama.sh
+++ scripts/train_obama.sh
@@ -1,8 +1,9 @@
 dataset=obama
 workspace=trial_obama
 asr_model=deepspeech
+
 python main.py data/$dataset --workspace $workspace -O --iters 100000 --asr_model $asr_model
 cp -r $workspace/checkpoints $workspace/checkpoints_
 python main.py data/$dataset --workspace $workspace -O --iters 125000 --finetune_lips --patch_size 32 --asr_model $asr_model
 python main.py data/$dataset --workspace $workspace -O --test --asr_model $asr_model
-# python main.py data/$dataset --workspace $workspace_torso/ -O --torso --head_ckpt ngp.pth --iters 200000
+# python main.py data/$dataset --workspace $workspace"_torso" -O --torso --head_ckpt ngp.pth --iters 200000
